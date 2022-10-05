1f46def15f9bd5b7d7723cfe7a5e90b86630dd39
Yasmim de Melo <yasmimdmf95@gmail.com>
2022-10-05 10:38:32 -0300

    Add models and settings for models

--- doc/models.py
+++ doc/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Documents(models.Model):
+    title = models.CharField(max_length=100, primary_key=True)
+    dt_criacao = models.DateTimeField(auto_now_add=True)
+    descricao = models.CharField(max_length=1000)
+
+    class Meta:
+        verbose_name_plural = 'Documents'
+
+    def __str__(self):
+        return self.title
