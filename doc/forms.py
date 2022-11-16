fd741a4140ab8842248e81d69f26862758bc010f
Yasmim <yasmimdmf95@gmail.com>
2022-11-16 18:07:42 -0300

    Add in forms def to remove label title and descricao

--- doc/forms.py
+++ doc/forms.py
@@ -1,7 +1,21 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Documents
 
-class DocumentsForm(ModelForm):
+class DocumentsForm(forms.ModelForm):
+    error_css_class = 'error-field'
+    required_css_class = 'required-field'
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Document title"}))
+    descricao = forms.CharField(label='', widget=forms.Textarea(attrs={"placeholder":"Descrição"}))
     class Meta:
         model = Documents
         fields = ['title', 'descricao']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].label=''
+        self.fields['title'].widget.attrs.update({'class': 'form-control-2'})
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['descricao'].label=''
+        self.fields['descricao'].widget.attrs.update({'class': 'form-control-2'})
