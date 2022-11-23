c376600abfd48c6ce5acc574de03ecee564923d2
Yasmim <yasmimdmf95@gmail.com>
2022-11-23 10:34:27 -0300

    Modified width form to responsive

--- doc/forms.py
+++ doc/forms.py
@@ -4,7 +4,7 @@ from .models import Documents
 class DocumentsForm(forms.ModelForm):
     error_css_class = 'error-field'
     required_css_class = 'required-field'
-    title = forms.CharField(label='', widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Document title"}))
+    title = forms.CharField(label='', widget=forms.TextInput(attrs={"class": "form-control", "placeholder": "Title"}))
     descricao = forms.CharField(label='', widget=forms.Textarea(attrs={"placeholder":"Descrição"}))
     class Meta:
         model = Documents
